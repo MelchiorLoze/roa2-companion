efba6ecb896b5c6e957115e9aecbfb2b697b7ed8
Melchior Lozé <melchior.loze@gmail.com>
2025-03-16 16:55:53 +0100

    added stats screen content v1

--- app/(private)/stats.tsx
+++ app/(private)/stats.tsx
@@ -1,5 +1,77 @@
-import { Text } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+
+import { usePlayerStats } from '@/hooks/business';
 
 export default function Stats() {
-  return <Text>Stats</Text>;
+  const { stats, isLoading } = usePlayerStats();
+
+  if (!stats || isLoading) return <ActivityIndicator color="white" size="large" style={styles.container} />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.title}>Ranked</Text>
+        <View>
+          <Text style={styles.label}>Elo: {stats.rankedElo}</Text>
+          <Text style={styles.label}>
+            Wins: {stats.rankedWinCount} - Losses: {stats.rankedMatchCount - stats.rankedWinCount}
+          </Text>
+          <Text style={styles.label}>Matches: {stats.rankedMatchCount}</Text>
+          <Text style={styles.label}>Winrate: {(stats.rankedWinRate ?? 0).toFixed(2)}%</Text>
+        </View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.title}>Global</Text>
+        <View>
+          <Text style={styles.label}>
+            Wins: {stats.globalWinCount} - Losses: {stats.globalMatchCount - stats.globalWinCount}
+          </Text>
+          <Text style={styles.label}>Matches: {stats.globalMatchCount}</Text>
+          <Text style={styles.label}>Winrate: {(stats.globalWinRate ?? 0).toFixed(2)}%</Text>
+        </View>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.title}>Character matches</Text>
+        <View>
+          <Text style={styles.label}>Clairen: {stats.claMatchCount}</Text>
+          <Text style={styles.label}>Etalus: {stats.etaMatchCount}</Text>
+          <Text style={styles.label}>Fleet: {stats.fleMatchCount}</Text>
+          <Text style={styles.label}>Forsburn: {stats.forMatchCount}</Text>
+          <Text style={styles.label}>Kragg: {stats.kraMatchCount}</Text>
+          <Text style={styles.label}>Loxodont: {stats.loxMatchCount}</Text>
+          <Text style={styles.label}>Maypul: {stats.mayMatchCount}</Text>
+          <Text style={styles.label}>Orcane: {stats.orcMatchCount}</Text>
+          <Text style={styles.label}>Ranno: {stats.ranMatchCount}</Text>
+          <Text style={styles.label}>Wrastor: {stats.wraMatchCount}</Text>
+          <Text style={styles.label}>Zetterburn: {stats.zetMatchCount}</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create((theme) => ({
+  container: {
+    flex: 1,
+    padding: theme.spacing.l,
+    gap: theme.spacing.l,
+  },
+  section: {
+    gap: theme.spacing.s,
+  },
+  title: {
+    fontFamily: theme.font.primary.italic,
+    fontSize: 18,
+    color: theme.color.white,
+    textTransform: 'uppercase',
+  },
+  label: {
+    fontFamily: theme.font.primary.regular,
+    fontSize: 16,
+    color: theme.color.white,
+    textTransform: 'uppercase',
+  },
+}));
--- hooks/business/index.ts
+++ hooks/business/index.ts
@@ -1,3 +1,4 @@
 export { useAuth } from './useAuth/useAuth';
 export { useCurrencyBalance } from './useCurrencyBalance/useCurrencyBalance';
+export { usePlayerStats } from './usePlayerStats/usePlayerStats';
 export { useRotatingCoinShop } from './useRotatingCoinShop/useRotatingCoinShop';
