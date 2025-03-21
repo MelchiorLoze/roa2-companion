24f243cff7030c63bb0300191b6825b707f17b91
Melchior Lozé <melchior.loze@gmail.com>
2025-03-22 00:14:42 +0100

    refactor section component

--- app/(private)/stats.tsx
+++ app/(private)/stats.tsx
@@ -1,4 +1,5 @@
 import { Image } from 'expo-image';
+import { PropsWithChildren } from 'react';
 import { ActivityIndicator, RefreshControl, ScrollView, Text, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
@@ -16,6 +17,15 @@ const getRankIcon = (elo: number) => {
   return MasterIcon;
 };
 
+type SectionProps = { title: string } & PropsWithChildren;
+
+const Section = ({ title, children }: SectionProps) => (
+  <View style={styles.section}>
+    <Text style={styles.title}>{title}</Text>
+    <View>{children}</View>
+  </View>
+);
+
 export default function Stats() {
   const { stats, refresh, isLoading } = usePlayerStats();
 
@@ -26,47 +36,38 @@ export default function Stats() {
       contentContainerStyle={styles.container}
       refreshControl={<RefreshControl onRefresh={refresh} refreshing={isLoading} />}
     >
-      <View style={styles.section}>
-        <Text style={styles.title}>Ranked (sets)</Text>
-        <View>
-          <View style={styles.labelWithIconContainer}>
-            <Text style={styles.label}>Elo: {stats.rankedElo}</Text>
-            <Image source={getRankIcon(stats.rankedElo)} style={styles.icon} />
-          </View>
-          <Text style={styles.label}>Matches: {stats.rankedMatchCount}</Text>
-          <Text style={styles.label}>
-            Wins: {stats.rankedWinCount} - Losses: {stats.rankedMatchCount - stats.rankedWinCount}
-          </Text>
-          <Text style={styles.label}>Winrate: {(stats.rankedWinRate ?? 0).toFixed(2)}%</Text>
+      <Section title="Ranked (sets)">
+        <View style={styles.labelWithIconContainer}>
+          <Text style={styles.label}>Elo: {stats.rankedElo}</Text>
+          <Image source={getRankIcon(stats.rankedElo)} style={styles.icon} />
         </View>
-      </View>
+        <Text style={styles.label}>Matches: {stats.rankedMatchCount}</Text>
+        <Text style={styles.label}>
+          Wins: {stats.rankedWinCount} - Losses: {stats.rankedMatchCount - stats.rankedWinCount}
+        </Text>
+        <Text style={styles.label}>Winrate: {(stats.rankedWinRate ?? 0).toFixed(2)}%</Text>
+      </Section>
 
-      <View style={styles.section}>
-        <Text style={styles.title}>Global (games)</Text>
-        <View>
-          <Text style={styles.label}>Matches: {stats.globalMatchCount}</Text>
-          <Text style={styles.label}>
-            Wins: {stats.globalWinCount} - Losses: {stats.globalMatchCount - stats.globalWinCount}
-          </Text>
-          <Text style={styles.label}>Winrate: {(stats.globalWinRate ?? 0).toFixed(2)}%</Text>
-        </View>
-      </View>
+      <Section title="Global (games)">
+        <Text style={styles.label}>Matches: {stats.globalMatchCount}</Text>
+        <Text style={styles.label}>
+          Wins: {stats.globalWinCount} - Losses: {stats.globalMatchCount - stats.globalWinCount}
+        </Text>
+        <Text style={styles.label}>Winrate: {(stats.globalWinRate ?? 0).toFixed(2)}%</Text>
+      </Section>
 
-      <View style={styles.section}>
-        <Text style={styles.title}>Per character (games)</Text>
-        <View>
-          {stats.gamesPlayedPerCharacter
-            .sort((a, b) => b.value - a.value)
-            .map((charStat) => (
-              <View key={charStat.character} style={styles.labelWithIconContainer}>
-                <Image source={CHARACTER_ICONS[charStat.character]} style={styles.icon} />
-                <Text style={styles.label}>
-                  {CHARACTER_NAMES[charStat.character]}: {charStat.value}
-                </Text>
-              </View>
-            ))}
-        </View>
-      </View>
+      <Section title="Per character (games)">
+        {stats.gamesPlayedPerCharacter
+          .sort((a, b) => b.value - a.value)
+          .map((charStat) => (
+            <View key={charStat.character} style={styles.labelWithIconContainer}>
+              <Image source={CHARACTER_ICONS[charStat.character]} style={styles.icon} />
+              <Text style={styles.label}>
+                {CHARACTER_NAMES[charStat.character]}: {charStat.value}
+              </Text>
+            </View>
+          ))}
+      </Section>
     </ScrollView>
   );
 }
