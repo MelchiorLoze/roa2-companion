3990dcb46605b4e9e2b8b53f4789a5302633fd85
Melchior Lozé <melchior.loze@gmail.com>
2025-03-21 23:03:49 +0100

    add icons to stats screen

--- app/(private)/stats.tsx
+++ app/(private)/stats.tsx
@@ -1,7 +1,38 @@
+import { Image } from 'expo-image';
 import { ActivityIndicator, RefreshControl, ScrollView, Text, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
+import {
+  BronzeIcon,
+  ClairenIcon,
+  DiamondIcon,
+  EtalusIcon,
+  FleetIcon,
+  ForsburnIcon,
+  GoldIcon,
+  KraggIcon,
+  LoxodontIcon,
+  MasterIcon,
+  MaypulIcon,
+  OrcaneIcon,
+  PlatinumIcon,
+  RannoIcon,
+  SilverIcon,
+  StoneIcon,
+  WrastorIcon,
+  ZetterburnIcon,
+} from '@/assets/images';
 import { usePlayerStats } from '@/hooks/business';
+
+const getRankIcon = (elo: number) => {
+  if (elo < 499) return StoneIcon;
+  if (elo < 699) return BronzeIcon;
+  if (elo < 899) return SilverIcon;
+  if (elo < 1099) return GoldIcon;
+  if (elo < 1299) return PlatinumIcon;
+  if (elo < 1499) return DiamondIcon;
+  return MasterIcon;
+};
 
 export default function Stats() {
   const { stats, refresh, isLoading } = usePlayerStats();
@@ -16,11 +47,14 @@ export default function Stats() {
       <View style={styles.section}>
         <Text style={styles.title}>Ranked (sets)</Text>
         <View>
-          <Text style={styles.label}>Elo: {stats.rankedElo}</Text>
+          <View style={styles.labelWithIconContainer}>
+            <Text style={styles.label}>Elo: {stats.rankedElo}</Text>
+            <Image source={getRankIcon(stats.rankedElo)} style={styles.icon} />
+          </View>
+          <Text style={styles.label}>Matches: {stats.rankedMatchCount}</Text>
           <Text style={styles.label}>
             Wins: {stats.rankedWinCount} - Losses: {stats.rankedMatchCount - stats.rankedWinCount}
           </Text>
-          <Text style={styles.label}>Matches: {stats.rankedMatchCount}</Text>
           <Text style={styles.label}>Winrate: {(stats.rankedWinRate ?? 0).toFixed(2)}%</Text>
         </View>
       </View>
@@ -28,10 +62,10 @@ export default function Stats() {
       <View style={styles.section}>
         <Text style={styles.title}>Global (games)</Text>
         <View>
+          <Text style={styles.label}>Matches: {stats.globalMatchCount}</Text>
           <Text style={styles.label}>
             Wins: {stats.globalWinCount} - Losses: {stats.globalMatchCount - stats.globalWinCount}
           </Text>
-          <Text style={styles.label}>Matches: {stats.globalMatchCount}</Text>
           <Text style={styles.label}>Winrate: {(stats.globalWinRate ?? 0).toFixed(2)}%</Text>
         </View>
       </View>
@@ -39,17 +73,50 @@ export default function Stats() {
       <View style={styles.section}>
         <Text style={styles.title}>Per character (games)</Text>
         <View>
-          <Text style={styles.label}>Clairen: {stats.claMatchCount}</Text>
-          <Text style={styles.label}>Etalus: {stats.etaMatchCount}</Text>
-          <Text style={styles.label}>Fleet: {stats.fleMatchCount}</Text>
-          <Text style={styles.label}>Forsburn: {stats.forMatchCount}</Text>
-          <Text style={styles.label}>Kragg: {stats.kraMatchCount}</Text>
-          <Text style={styles.label}>Loxodont: {stats.loxMatchCount}</Text>
-          <Text style={styles.label}>Maypul: {stats.mayMatchCount}</Text>
-          <Text style={styles.label}>Orcane: {stats.orcMatchCount}</Text>
-          <Text style={styles.label}>Ranno: {stats.ranMatchCount}</Text>
-          <Text style={styles.label}>Wrastor: {stats.wraMatchCount}</Text>
-          <Text style={styles.label}>Zetterburn: {stats.zetMatchCount}</Text>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={ClairenIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.claMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={EtalusIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.etaMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={FleetIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.fleMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={ForsburnIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.forMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={KraggIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.kraMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={LoxodontIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.loxMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={MaypulIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.mayMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={OrcaneIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.orcMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={RannoIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.ranMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={WrastorIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.wraMatchCount}</Text>
+          </View>
+          <View style={styles.labelWithIconContainer}>
+            <Image source={ZetterburnIcon} style={styles.icon} />
+            <Text style={styles.label}>{stats.zetMatchCount}</Text>
+          </View>
         </View>
       </View>
     </ScrollView>
@@ -58,7 +125,6 @@ export default function Stats() {
 
 const styles = StyleSheet.create((theme) => ({
   container: {
-    flex: 1,
     padding: theme.spacing.l,
     gap: theme.spacing.l,
   },
@@ -76,5 +142,14 @@ const styles = StyleSheet.create((theme) => ({
     fontSize: 16,
     color: theme.color.white,
     textTransform: 'uppercase',
+  },
+  labelWithIconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: theme.spacing.xs,
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
 }));
--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,6 +1,31 @@
-export const CoinsIcon = require('./coins.png');
-export const BucksIcon = require('./bucks.png');
-export const CommonIcon = require('./common.webp');
-export const RareIcon = require('./rare.webp');
-export const EpicIcon = require('./epic.webp');
-export const LegendaryIcon = require('./legendary.webp');
+// CURRENCIES
+export const CoinsIcon = require('./currency/coins.png');
+export const BucksIcon = require('./currency/bucks.png');
+
+// RARITIES
+export const CommonIcon = require('./rarity/common.webp');
+export const RareIcon = require('./rarity/rare.webp');
+export const EpicIcon = require('./rarity/epic.webp');
+export const LegendaryIcon = require('./rarity/legendary.webp');
+
+// RANKS
+export const StoneIcon = require('./rank/stone.png');
+export const BronzeIcon = require('./rank/bronze.png');
+export const SilverIcon = require('./rank/silver.png');
+export const GoldIcon = require('./rank/gold.png');
+export const PlatinumIcon = require('./rank/platinum.png');
+export const DiamondIcon = require('./rank/diamond.png');
+export const MasterIcon = require('./rank/master.png');
+
+// CHARACTERS
+export const ClairenIcon = require('./character/clairen.png');
+export const EtalusIcon = require('./character/etalus.png');
+export const ForsburnIcon = require('./character/forsburn.png');
+export const FleetIcon = require('./character/fleet.png');
+export const KraggIcon = require('./character/kragg.png');
+export const LoxodontIcon = require('./character/loxodont.png');
+export const MaypulIcon = require('./character/maypul.png');
+export const OrcaneIcon = require('./character/orcane.png');
+export const RannoIcon = require('./character/ranno.png');
+export const WrastorIcon = require('./character/wrastor.png');
+export const ZetterburnIcon = require('./character/zetterburn.png');
