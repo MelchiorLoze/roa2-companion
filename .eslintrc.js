9ecd1afba3a2312fd5734f18404d9f2a9e0efa3e
Melchior Lozé <melchior.loze@gmail.com>
2025-03-22 23:39:55 +0100

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', 'plugin:react-native/all', 'plugin:react/recommended'],
   ignorePatterns: ['/dist/*'],
   plugins: ['simple-import-sort'],
   rules: {
+    'react/react-in-jsx-scope': 'off', // Not necessary after React 17
+    'react-native/sort-styles': 'off',
     'simple-import-sort/imports': 'warn',
     'simple-import-sort/exports': 'warn',
     'react/jsx-sort-props': 'warn',
