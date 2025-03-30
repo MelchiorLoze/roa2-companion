00daa93dac6279eac4c14cc6aff8c0069952da7f
Melchior Lozé <melchior.loze@gmail.com>
2025-03-30 20:38:01 +0200

    add tests for ItemList

---
+++ components/ItemList/ItemList.test.tsx
@@ -0,0 +1,58 @@
+import { fireEvent, render, screen, within } from '@testing-library/react-native';
+
+import { Item, Rarity } from '@/types/store';
+
+import { ItemList } from './ItemList';
+
+const items: Item[] = [
+  {
+    id: '1',
+    title: 'Item 1',
+    rarity: Rarity.COMMON,
+    category: 'icon',
+    coinPrice: 2000,
+    buckPrice: 20,
+  },
+  {
+    id: '2',
+    title: 'Item 2',
+    rarity: Rarity.EPIC,
+    category: 'skin',
+    coinPrice: 50000,
+    buckPrice: 500,
+  },
+];
+const onSelectMock = jest.fn();
+
+const renderComponent = (items: Item[]) => {
+  render(<ItemList items={items} onSelect={onSelectMock} />);
+
+  expect(onSelectMock).not.toHaveBeenCalled();
+};
+
+describe('ItemList', () => {
+  it('should render correctly an empty item list', () => {
+    renderComponent([]);
+
+    expect(screen.queryByRole('button')).toBeNull();
+  });
+
+  it('should render correctly items from the list', () => {
+    renderComponent(items);
+
+    const itemCards = screen.getAllByRole('button');
+    expect(itemCards).toHaveLength(items.length);
+    within(itemCards[0]).getByText(items[0].title);
+    within(itemCards[1]).getByText(items[1].title);
+  });
+
+  it('should call onSelect when an item is pressed', () => {
+    renderComponent(items);
+
+    const itemCards = screen.getAllByRole('button');
+    fireEvent.press(itemCards[0]);
+
+    expect(onSelectMock).toHaveBeenCalledTimes(1);
+    expect(onSelectMock).toHaveBeenCalledWith(items[0]);
+  });
+});
