23a45862b46d160a7e66e086750c89cccfbf3782
Melchior Lozé <melchior.loze@gmail.com>
2025-06-14 11:44:08 +0200

    add tests for disclaimer

---
+++ src/components/Disclaimer/Disclaimer.test.tsx
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import { useRouter } from 'expo-router';
+
+import { Disclaimer } from './Disclaimer';
+
+const disclaimerText = `This project is a fan-made creation and is not affiliated with Aether Studios or Offbrand Games in any official capacity. The app works directly between your device and the official game servers, with no third parties involved in transferring or processing your data. Learn more...`;
+
+jest.mock('expo-router');
+const useRouterMock = jest.mocked(useRouter);
+const navigateMock = jest.fn();
+useRouterMock.mockReturnValue({
+  navigate: navigateMock,
+} as unknown as ReturnType<typeof useRouter>);
+
+describe('Disclaimer', () => {
+  it('renders correctly', () => {
+    render(<Disclaimer />);
+
+    screen.getByText(disclaimerText);
+  });
+
+  it('navigates to /about on press', () => {
+    render(<Disclaimer />);
+
+    const disclaimerButton = screen.getByText(disclaimerText);
+    fireEvent.press(disclaimerButton);
+
+    expect(navigateMock).toHaveBeenCalledTimes(1);
+    expect(navigateMock).toHaveBeenCalledWith('/about');
+  });
+});
