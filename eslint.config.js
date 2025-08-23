63334a41e8e1b6e052f08c16286473bb0a6520ac
Melchior Lozé <melchior.loze@gmail.com>
2025-08-23 18:30:13 +0200

    chore: Finalize switch from CommonJS to ESM (#9)

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,26 @@
-const { defineConfig, globalIgnores } = require('eslint/config');
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactNative from 'eslint-plugin-react-native';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const reactNative = require('eslint-plugin-react-native');
-const react = require('eslint-plugin-react');
-const typescriptEslint = require('@typescript-eslint/eslint-plugin');
-const simpleImportSort = require('eslint-plugin-simple-import-sort');
-const tsParser = require('@typescript-eslint/parser');
-const js = require('@eslint/js');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const { FlatCompat } = require('@eslint/eslintrc');
+const { configs } = js;
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+  recommendedConfig: configs.recommended,
+  allConfig: configs.all,
 });
 
-module.exports = defineConfig([
+export default defineConfig([
   {
     extends: compat.extends(
       'expo',
@@ -34,7 +39,6 @@ module.exports = defineConfig([
 
     languageOptions: {
       parser: tsParser,
-
       parserOptions: {
         projectService: true,
       },
@@ -42,13 +46,16 @@ module.exports = defineConfig([
 
     rules: {
       'react-native/sort-styles': 'off',
+
       'react/jsx-sort-props': 'warn',
       'react/react-in-jsx-scope': 'off',
       'react/prefer-read-only-props': 'warn',
-      '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
-      '@typescript-eslint/consistent-type-imports': 'warn',
-      '@typescript-eslint/consistent-type-exports': 'warn',
 
+      '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+      '@typescript-eslint/consistent-type-exports': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-require-imports': ['error', { allow: ['\\.png$', '\\.otf$', '\\.ttf$'] }],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
@@ -57,12 +64,10 @@ module.exports = defineConfig([
           caughtErrorsIgnorePattern: '^_',
         },
       ],
-
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/require-await': 'off',
-      'simple-import-sort/imports': 'warn',
+
       'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': 'warn',
 
       'import/no-restricted-paths': [
         'warn',
@@ -80,5 +85,5 @@ module.exports = defineConfig([
       ],
     },
   },
-  globalIgnores(['dist/*', '**/*.js']),
+  globalIgnores(['dist/*', '**/*.js', '**/*.cjs']),
 ]);
