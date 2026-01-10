bf609d7cf84a5b297be74d9f6bdec1178924198f
Melchior Lozé <melchior.loze@gmail.com>
2026-01-10 22:56:36 +0100

    Include sponsorship option (#26)

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
-import { GitHubIcon } from '@/assets/images/link';
+import { GitHubIcon, KoFiIcon } from '@/assets/images/link';
 import { ActionRow } from '@/components/ActionRow/ActionRow';
 import { Separator } from '@/components/Separator/Separator';
 
@@ -27,6 +27,13 @@ export default function About() {
         label="GitHub"
         logo={GitHubIcon}
         url={new URL('https://github.com/MelchiorLoze/roa2-companion')}
+      />
+      <Separator />
+      <ActionRow
+        iconName="arrow-outward"
+        label="Support the project"
+        logo={KoFiIcon}
+        url={new URL('https://ko-fi.com/roa2companion')}
       />
       <Separator />
     </View>
--- src/assets/images/link/index.ts
+++ src/assets/images/link/index.ts
@@ -3,4 +3,5 @@ import { type ImageSource } from 'expo-image';
 export const DiscordIcon: ImageSource = require('./discord.png');
 export const DragdownIcon: ImageSource = require('./dragdown.png');
 export const GitHubIcon: ImageSource = require('./github.png');
+export const KoFiIcon: ImageSource = require('./ko-fi.png');
 export const RedditIcon: ImageSource = require('./reddit.png');
