a5051c2dff84684812a46d29463005785d0f8c0d
Melchior Lozé <melchior.loze@gmail.com>
2026-01-16 16:55:54 +0100

    improve store item frames

--- src/assets/images/rarity/index.ts
+++ src/assets/images/rarity/index.ts
@@ -3,7 +3,7 @@ import { type ImageSource } from 'expo-image';
 export const FrameCommonBack: ImageSource = require('./frame_common_back.png');
 export const FrameRareBack: ImageSource = require('./frame_rare_back.png');
 export const FrameEpicBack: ImageSource = require('./frame_epic_back.png');
-export const FrameLegendaryBack: ImageSource = require('./frame_epic_back.png'); // TODO: Add legendary back frame
+export const FrameLegendaryBack: ImageSource = require('./frame_legendary_back.png');
 
 export const FrameCommonFront: ImageSource = require('./frame_common_front.png');
 export const FrameRareFront: ImageSource = require('./frame_rare_front.png');
