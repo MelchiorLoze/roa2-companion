fe635e3bb12acf816f39b7ee0ac30cc7eb1c74a6
Melchior Lozé <melchior.loze@gmail.com>
2026-01-16 16:54:59 +0100

    Change background

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { type NativeStackHeaderProps } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ImageBackground } from 'expo-image';
 import * as NavigationBar from 'expo-navigation-bar';
 import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StrictMode, useEffect } from 'react';
 import { useUnistyles } from 'react-native-unistyles';
 
+import { AppBackground } from '@/assets/images';
 import { Header } from '@/components/Header/Header';
 import { SessionProvider } from '@/features/auth/contexts/SessionContext/SessionContext';
 import { useAppFonts } from '@/hooks/core/useAppFonts/useAppFonts';
@@ -31,18 +33,20 @@ export default function RootLayout() {
           {/* Ensure content is drawn under the status bar */}
           <StatusBar backgroundColor="transparent" style="light" translucent />
 
-          <Stack
-            initialRouteName="sign-in"
-            screenOptions={{
-              header: renderHeader,
-              contentStyle: { backgroundColor: theme.color.background },
-              animation: 'slide_from_right',
-            }}
-          >
-            <Stack.Screen name="sign-in" />
-            <Stack.Screen name="about" options={{ title: 'About this app' }} />
-            <Stack.Screen name="(private)" options={{ headerShown: false }} />
-          </Stack>
+          <ImageBackground source={AppBackground} style={{ flex: 1 }}>
+            <Stack
+              initialRouteName="sign-in"
+              screenOptions={{
+                header: renderHeader,
+                contentStyle: { backgroundColor: theme.color.transparent },
+                animation: 'slide_from_right',
+              }}
+            >
+              <Stack.Screen name="sign-in" />
+              <Stack.Screen name="about" options={{ title: 'About this app' }} />
+              <Stack.Screen name="(private)" options={{ headerShown: false }} />
+            </Stack>
+          </ImageBackground>
         </SessionProvider>
       </QueryClientProvider>
     </StrictMode>
