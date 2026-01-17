18a7a177fd289e5574b5f2309ad6ac6a5c32e191
Melchior Lozé <melchior.loze@gmail.com>
2026-01-18 00:43:19 +0100

    fix file names

--- src/assets/images/rarity/index.ts
+++ src/assets/images/rarity/index.ts
@@ -1,11 +1,11 @@
 import { type ImageSource } from 'expo-image';
 
-export const FrameCommonBack: ImageSource = require('./frame_common_back.png');
-export const FrameRareBack: ImageSource = require('./frame_rare_back.png');
-export const FrameEpicBack: ImageSource = require('./frame_epic_back.png');
-export const FrameLegendaryBack: ImageSource = require('./frame_legendary_back.png');
+export const FrameCommonBack: ImageSource = require('./frame-common-back.png');
+export const FrameRareBack: ImageSource = require('./frame-rare-back.png');
+export const FrameEpicBack: ImageSource = require('./frame-epic-back.png');
+export const FrameLegendaryBack: ImageSource = require('./frame-legendary-back.png');
 
-export const FrameCommonFront: ImageSource = require('./frame_common_front.png');
-export const FrameRareFront: ImageSource = require('./frame_rare_front.png');
-export const FrameEpicFront: ImageSource = require('./frame_epic_front.png');
-export const FrameLegendaryFront: ImageSource = require('./frame_legendary_front.png');
+export const FrameCommonFront: ImageSource = require('./frame-common-front.png');
+export const FrameRareFront: ImageSource = require('./frame-rare-front.png');
+export const FrameEpicFront: ImageSource = require('./frame-epic-front.png');
+export const FrameLegendaryFront: ImageSource = require('./frame-legendary-front.png');
