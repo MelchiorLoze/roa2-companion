414d31be98addb4d2b85c43f1ecf6ed8622c0df2
Melchior Lozé <melchior.loze@gmail.com>
2026-04-25 21:08:47 +0200

    revamp currencies

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -2,6 +2,7 @@ import { type FontSource } from 'expo-font';
 
 export const AgencyFBBlack: FontSource = require('./AgencyFB-Black.otf');
 export const AgencyFBBold: FontSource = require('./AgencyFB-Bold.ttf');
+export const AgencyFBBoldWide: FontSource = require('./AgencyFB-BoldWide.ttf');
 
 export const FranklinGothicDemiCondItalic: FontSource = require('./FranklinGothicDemiCond-Italic.otf');
 export const FranklinGothicDemiCondRegular: FontSource = require('./FranklinGothicDemiCond-Regular.ttf');
