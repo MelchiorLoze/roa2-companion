dd3bdd48e8795446137ec4ae081e7cec32dc2db8
Melchior Lozé <melchior.loze@gmail.com>
2026-04-25 18:20:32 +0200

    revamp dialogs and inputs

--- src/assets/images/ui/index.ts
+++ src/assets/images/ui/index.ts
@@ -1,5 +1,6 @@
 import { type ImageSource } from 'expo-image';
 
 export const ButtonBackground: ImageSource = require('./button-bg.png'); // t_ui_GenericSmallButton
+export const DialogBackground: ImageSource = require('./dialog-bg.png'); // MI_StoreItemRippleBG
 export const ItemOutline: ImageSource = require('./item-outline.png'); // DarkPurpleStoreItemOutline
 export const ItemBackground: ImageSource = require('./item-bg.png'); // MI_StoreItemRippleBG
