ab952cde31e3e7ccf6bd3a4dba44617b6fb2edc1
Melchior Lozé <melchior.loze@gmail.com>
2026-04-25 18:20:32 +0200

    revamp dialogs and inputs

--- src/features/store/components/PurchaseConfirmationDialog/PurchaseConfirmationDialog.tsx
+++ src/features/store/components/PurchaseConfirmationDialog/PurchaseConfirmationDialog.tsx
@@ -25,12 +25,7 @@ const Content = ({ item, onClose }: Readonly<Props>) => {
     });
   };
 
-  if (isLoading)
-    return (
-      <View style={styles.spinner}>
-        <Spinner />
-      </View>
-    );
+  if (isLoading) return <Spinner />;
 
   if (isError) {
     return (
@@ -85,6 +80,7 @@ const styles = StyleSheet.create((theme) => ({
     textAlign: 'center',
   },
   buttonContainer: {
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
