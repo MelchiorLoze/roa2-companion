8dfb44b9318be14b00fe6f8fbcd358f391f2ecc9
Melchior Lozé <melchior.loze@gmail.com>
2026-05-09 21:58:06 +0200

    revamp coin store countdown

--- src/assets/images/ui/icons/index.ts
+++ src/assets/images/ui/icons/index.ts
@@ -2,3 +2,4 @@ import { type ImageSource } from 'expo-image';
 
 export const ClockIcon: ImageSource = require('./clock.png'); // time_icon
 export const LeftArrowIcon: ImageSource = require('./left-arrow.png'); // UILeftArrow1
+export const TimeIcon: ImageSource = require('./time.png');
